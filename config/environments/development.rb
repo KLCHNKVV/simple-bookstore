228b7dfb7eca9a371bcc7c9591cf878d34958fa8
klchnkvv <daniel.kliuchnikov@mail.ru>
2018-05-29 05:54:52 +0300

    Minor fixes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,11 +57,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'https://your-bookshelf.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      :address              => 'smtp.sendgrid.net',
+      :address              => 'smtp.gmail.com',
       :port                 => 587,
-      :domain               => 'heroku.com',
-      :user_name            => ENV['SENDGRID_USERNAME'],
-      :password             => ENV['SENDGRID_PASSWORD'],
+      :domain               => 'gmail.com',
+      :user_name            => 'klchnkvv@gmail.com',
+      :password             => 'ariastalver476dc7991cde_rfvbgt',
       :authentication       => 'plain',
       :enable_starttls_auto => true
   }
